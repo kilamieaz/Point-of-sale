44156c08c4de8652d995b4937a27e19afc3d1543
kilamieaz <im.sultaan05@gmail.com>
2018-10-27 09:20:55 +0700

    home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.master')
+@section('title')
+  Home
+@endsection
 
+@section('breadcrumb')
+   @parent
+   <li>Dashboard</li>
+@endsection
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<!-- Default box -->
+<div class="box">
+    <div class="box-header with-border">
+        <h3 class="box-title">Title</h3>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                title="Collapse">
+                <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip"
+                title="Remove">
+                <i class="fa fa-times"></i></button>
         </div>
     </div>
+    <div class="box-body">
+    </div>
+    <!-- /.box-body -->
+    <div class="box-footer">
+    </div>
+    <!-- /.box-footer-->
 </div>
+<!-- /.box -->
 @endsection
