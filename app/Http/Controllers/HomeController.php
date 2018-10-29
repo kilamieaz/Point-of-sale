a1cab6560b667f2639e3483dc7481ab99f1dd93b
kilamieaz <im.sultaan05@gmail.com>
2018-10-29 07:13:32 +0700

    view dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (Auth::user()->level == 1) {
+            return view('home.admin');
+        }
+        return view('home.kasir');
     }
 }
