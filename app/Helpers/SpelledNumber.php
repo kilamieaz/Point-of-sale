408a5ac8023338e2012eafebcc7762089daf2a2c
kilamieaz <im.sultaan05@gmail.com>
2018-11-05 07:44:44 +0700

    change helper spelled number

--- app/Helpers/SpelledNumber.php
+++ app/Helpers/SpelledNumber.php
@@ -1,47 +1,46 @@
 <?php
-function spelled_number($currency) {
-    $angka = array(false, 'satu ','dua ','tiga ','empat ','lima ','enam ','tujuh ','delapan ','sembilan ');
-    $group = array(2 => 'ribu ', 3 => 'juta ', 4=> 'miliar ', 5 => 'triliyun ', 6 => 'kuadriliun ');
-    $satuan = array(2 => 'puluh ', 3 => 'ratus ');
-     
-    $n = strlen($currency);
-    $g = ceil($n/3);
-    $currency = str_pad($currency,$g*3,'0',STR_PAD_LEFT);
-     
-    for($x=0; $x<$g; $x++) {
-    $t = $g - $x;
-    $c = ltrim(substr($currency,$x*3, 3),'0');
-    $sub = false;
-     
-    $n = strlen($c);
-    for($i=0; $i < $n; $i++) {
-    $rpos = $n - $i;
-    $gpos = (!$gpos = ($rpos % 3)) ? 3: $gpos;
-     
-    $a = $c[$i];
-    $s = false;
-    if($gpos==1 ) {
-    $a = ($c[$i-1]==1 || !$a) ? false: $a;
-    $a = ($a==1 && ($t==2 && $c==1)) ? 'se': $angka[$a];
+function spelled_number($number)
+{
+    $result = '';
+    $number = strval($number);
+    if (!preg_match('/^[0-9]{1,15}$/', $number)) {
+        return false;
     }
-    elseif($gpos==2 && $a==1) {
-    $a = ($c[$i+1]>1) ? $angka[$c[$i+1]]: 'se';
-    $s = ($c[$i+1]>0) ? 'belas ': $satuan[$gpos];
+    $ones = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan'];
+    $majorUnits = ['', 'ribu', 'juta', 'milyar', 'trilyun'];
+    $minorUnits = ['', 'puluh', 'ratus'];
+    $length = strlen($number);
+    $isAnyMajorUnit = false;
+
+    for ($i = 0, $pos = $length - 1; $i < $length; $i++, $pos--) {
+        if ($number{$i} != '0') {
+            if ($number{$i} != '1') {
+                $result .= $ones[$number{$i}] . ' ' . $minorUnits[$pos % 3] . ' ';
+            } elseif ($pos % 3 == 1 && $number{$i + 1} != '0') {
+                if ($number{$i + 1} == '1') {
+                    $result .= 'sebelas ';
+                } else {
+                    $result .= $ones[$number{$i + 1}] . ' belas ';
+                }
+                $i++;
+                $pos--;
+            } elseif ($pos % 3 != 0) {
+                $result .= 'se' . $minorUnits[$pos % 3] . ' ';
+            } elseif ($pos == 3 && !$isAnyMajorUnit) {
+                $result .= 'se';
+            } else {
+                $result .= 'satu ';
+            }
+            $isAnyMajorUnit = true;
+        }
+        if ($pos % 3 == 0 && $isAnyMajorUnit) {
+            $result .= $majorUnits[$pos / 3] . ' ';
+            $isAnyMajorUnit = false;
+        }
     }
-    elseif($gpos==2 && $a!=1) {
-    $s = ($a) ? $satuan[$gpos]: false;
-    $a = $angka[$a];
+    $result = trim($result);
+    if ($result == '') {
+        $result = 'nol';
     }
-    else {
-    $s = ($a) ? $satuan[$gpos]: false;
-    $a = ($a==1) ? 'se': $angka[$a];
-    }
-     
-    $sub.= $a.$s;
-    }
-     
-    $str.= $sub.(($c) ? $group[$t]: false);
-    }
-     
-    return $str;
-    }
+    return $result;
+}
